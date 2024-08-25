e4dafe6cd5b21bce428bd5362f9d6ade28f0cec4
My Name <jjocel93@gmail.com>
2024-08-25 23:14:21 +0200

    complete lessons

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (book) {
+  const title = books.map((book) => book.title);
+  return title;
 };
 
+const books = [
+  {
+    title: 'Book',
+    author: 'Name',
+  },
+  {
+    title: 'Book2',
+    author: 'Name2',
+  },
+];
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+  person.yearOfDeath ??= new Date().getFullYear();
 
+  return person.yearOfDeath - person.yearOfBirth;
 };
+
+const findTheOldest = function (people) {
+  const ordered = people.sort((a, b) => (getAge(a) > getAge(b) ? 1 : -1));
+
+  const biggest = ordered[2];
+  return biggest;
+};
+
+// const findTheOldest = function (people) {
+//   const ordered = people.sort((a, b) =>
+//     a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? 1 : -1
+//   );
+
+//   const biggest = ordered[2];
+//   return biggest;
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
